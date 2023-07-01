4c4543d28ef9c32a992cb8e8eeda2795dd170aec
InhyeKang <kanginhae0826@gmail.com>
2023-07-01 22:17:31 +0900

    ⚙️ :: 레포 변경 후 프로젝트 재세팅

---
+++ MYLOG_iOS/MYLOG_iOS/MYLOG_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MYLOG_iOSApp.swift
+//  MYLOG_iOS
+//
+//  Created by 강인혜 on 1/7/23.
+//
+
+import SwiftUI
+
+@main
+struct MYLOG_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
