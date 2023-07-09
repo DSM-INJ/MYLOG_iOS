ddfac3cf3b446caf6be1e6b69118b3cd0d68ec1d
InhyeKang <kanginhae0826@gmail.com>
2023-07-09 23:30:26 +0900

    🌱 :: Half Modal

--- MYLOG_iOS/MYLOG_iOS/Home/HomeView.swift
+++ MYLOG_iOS/MYLOG_iOS/Home/HomeView.swift
@@ -8,9 +8,22 @@
 import SwiftUI
 
 struct HomeView: View {
+    @State var isShowingSheet = true
     var body: some View {
         VStack(alignment: .center) {
             CustomCalendar()
         }
+        .sheet(isPresented: $isShowingSheet) {
+            HalfSheet {
+                Text("hello")
+            }
+            .shadow(color: .black, x: 0, y: -2, blur: 20, spread: 0)
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
     }
 }
